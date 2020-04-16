d147e839e3a71567c2a7c4aef475761e2997d31c
Phecda <szp12345141@gmail.com>
2020-04-16 15:27:00 +0800

    fix(android): fix immer es5 error when using Hermes
    https://immerjs.github.io/immer/docs/installation#immer-on-older-javascript-environments

--- App.tsx
+++ App.tsx
@@ -11,8 +11,16 @@
 import React from 'react';
 import { DarkModeProvider } from 'react-native-dark-mode';
 import { enableScreens } from 'react-native-screens';
+import { enableES5 } from 'immer';
 import AppNavigationContainer from './src/screen/AppNavigationContainer';
 import { I18NProvider } from './src/i18n';
+import { Platform } from 'react-native';
+
+declare var global: { HermesInternal: null | {} };
+if (Platform.OS === 'android' && global.HermesInternal) {
+  // https://immerjs.github.io/immer/docs/installation#immer-on-older-javascript-environments
+  enableES5();
+}
 
 if (__DEV__) {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
